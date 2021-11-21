596ed0948091ffb614449533520b8430a90efafa
hirotaka42 <kodama.hirotaka.190380@roivy.net>
2021-11-21 22:06:02 +0900

    Python3-Downloader, first commit

---
+++ start.py
@@ -0,0 +1,70 @@
+
+import yt_dlp
+
+# フォルダーを開くために必要
+import subprocess
+import os
+
+
+def is_open_dir():
+    """
+    現在のディレクトリを開く
+    """
+
+    # フォルダのパスを取得
+    path = os.getcwd()
+    # open
+    subprocess.call(["open", path])
+
+
+def is_imput() -> str:
+    """
+    DLするURLを入力処理する
+    """
+    key = ""
+    print("+-------------------------------------+")
+    print("URLを入力してください(終了するには'n'を入力)")
+    print("+-------------------------------------+")
+    key = input("> ")
+
+    print('{}'.format(key) + " が入力されました")
+
+    return key
+
+
+def is_download_finish():
+    """
+    デコレーター
+    DL完了したらディレクトリを開く
+    """
+    print("+-------------------------------------+")
+    print("Done downloading! ")
+    
+    
+
+def main():
+    """
+    メイン関数
+    入力されたURLから動画を保存する
+    """
+
+    imput_key = is_imput()
+    if imput_key[:1] == "n" or imput_key[:1] == "N":
+        print("ディレクトリを開き,終了します")
+        # ディレクトリを開く
+        is_open_dir()
+
+        exit()
+
+    ydl_opts = {}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(['{}'.format(imput_key)])
+        
+    is_download_finish()
+
+
+if __name__ == '__main__' :
+
+    while 1 :
+        main()
+
